6bed77f0d658484ffe66dfdfeb943abc06057104
Noah <noah@noah.kr>
2018-06-18 21:44:37 +0900

    Stephen grider's GraphQL lecture

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://noah2021:noah2021@ds247170.mlab.com:47170/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
