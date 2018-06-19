d9a2e4e2b2fbe4ac0c8eaeeb60bd2754be86c116
Noah <noah@noah.kr>
2018-06-19 21:08:27 +0900

    before Lyrics list

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -5,15 +5,18 @@ import { Link } from 'react-router';
 import query from '../queries/fetchSongs';
 
 class SongList extends Component {
-  onSongDelete() {
-    this.props.mutate({ variables: { id } });
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } })
+      .then(() => this.props.data.refetch());
   }
 
   renderSongs() {
     return this.props.data.songs.map(({ id, title }) => {
       return (
         <li key={id} className="collection-item">
+          <Link to={`/songs/${id}`}>
           {title}
+          </Link>
           <i
             className="material-icons"
             onClick={() => this.onSongDelete(id)}
